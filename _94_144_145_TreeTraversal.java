cb0b7e9c7638744db3d049629e7a9e27f8b628a8
rkarmaka98 <rkarmaka98@gmail.com>
2023-07-06 22:10:38 +0530

    Iterative approach tree traversal Preorder

--- _94_144_145_TreeTraversal.java
+++ _94_144_145_TreeTraversal.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+import java.util.List;
+import java.util.Deque;
 
 public class _94_144_145_TreeTraversal {
     //Recursive Approaches O(N)--->T, O(N)--->S takes advantages of recursion call stack which memorizes nodes.
@@ -63,6 +64,45 @@ public class _94_144_145_TreeTraversal {
             dfs(node.left);
             dfs(node.right);
             list.add(node.val);
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+
+    //Iterative Approaches O(N)--->T, O(N)--->S takes advantages of recursion call stack which memorizes nodes.
+
+    /*
+    Preorder Traversal
+     */
+    class Solution {
+        List<Integer> list=new ArrayList<>();
+        Deque<TreeNode> stack=new ArrayDeque<>();
+
+        public List<Integer> preorderTraversal(TreeNode root) {
+            dfs(root);
+            return list;
+        }
+
+        public void dfs(TreeNode node){
+            //edge case when the root node is null as it throws exception in push method.
+            if(node==null){
+                return;
+            }
+            stack.push(node);
+            while(!stack.isEmpty()){
+                //visit the current node
+                TreeNode currNode=stack.pop();
+                list.add(currNode.val);
+
+                //Check the null condition as if has no children node.
+                //We push right node first as it is stack data structure LIFO
+                if(currNode.right!=null){
+                    stack.push(currNode.right);
+                }
+                if(currNode.left!=null){
+                    stack.push(currNode.left);
+                }
+            }
         }
     }
 }
