767e955e30643ebec903d71e941e3fdeca57ecd7
rkarmaka98 <rkarmaka98@gmail.com>
2023-07-06 23:39:22 +0530

    Morris Preorder Traversal O(1) space complexity.

--- _94_144_145_TreeTraversal.java
+++ _94_144_145_TreeTraversal.java
@@ -105,4 +105,52 @@ public class _94_144_145_TreeTraversal {
             }
         }
     }
+
+    //Morris threaded Tree Traversal  O(N)--->T, O(1)--->S Instead of storing previous node information on stack frame
+    //keep track of only current and last nodes
+    class Solution {
+        List<Integer> list=new ArrayList<>();
+
+        public List<Integer> preorderTraversal(TreeNode root) {
+            dfsMorris(root);
+            return list;
+        }
+
+        public void dfsMorris(TreeNode root){
+            //Keep track of curr and last nodes; Initialize curr as root node.
+            TreeNode curr=root;
+            TreeNode last;
+
+            while(curr!=null){
+                //If left node is not null find the right most node of that left subtree
+                if(curr.left!=null){
+                    //initialize last node as left node and iterate until rightmost last node
+                    last=curr.left;
+                    //if right node is curr means we already traversed it
+                    while(last.right!=null && last.right!=curr){
+                        last=last.right;
+                    }
+                    //if right pointer of last node is null visit current node and add the thread to curr node
+                    //proceeds to traverse left subtree
+                    if(last.right==null){
+                        list.add(curr.val);
+                        last.right=curr;
+                        curr=curr.left;
+                    }
+                    //If right pointer of last is not null so we set as null as already traversed and proceeds to right
+                    //Subtree Iteration
+                    else{
+                        last.right=null;
+                        curr=curr.right;
+                    }
+
+                }
+                //If left subtree is not present visit curr and proceeds to right subtree
+                else{
+                    list.add(curr.val);
+                    curr=curr.right;
+                }
+            }
+        }
+    }
 }
